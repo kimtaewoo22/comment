efb2018368b2718ccb6fe90b4f06944690d13b71
tw <tw@localhost>
2021-09-16 16:30:07 +0900

    댓글 프로젝트 생

---
+++ src/main/java/com/example/lol/mapper/CommentMapper.java
@@ -0,0 +1,11 @@
+package com.example.lol.mapper;
+
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CommentMapper {
+	public Map<String, Object> getComment(Map<String, Object> params);
+	
+}
