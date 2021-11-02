eee69af1fc8f407e58b442579c7c6bf18ec9ce57
tw <tw@218.38.137.124>
2021-11-02 15:30:07 +0900

    필요없는 import 제거

--- src/main/java/com/example/lol/controller/CategoryController.java
+++ src/main/java/com/example/lol/controller/CategoryController.java
@@ -1,8 +1,5 @@
 package com.example.lol.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
