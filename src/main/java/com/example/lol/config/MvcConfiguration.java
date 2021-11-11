a3d9fbca8d72638f0b1524cb7da81463ea7d0606
tw <tw@218.38.137.124>
2021-11-11 17:02:58 +0900

    url pattern 필터설정

---
+++ src/main/java/com/example/lol/config/MvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.lol.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.lol.interceptor.CommentInterceptor;
+
+@Configuration
+public class MvcConfiguration implements WebMvcConfigurer {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	CommentInterceptor commentInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		logger.debug("addInterceptors.........start");
+		registry.addInterceptor(commentInterceptor).addPathPatterns("/**").excludePathPatterns("/**/login").excludePathPatterns("/**/token");
+	}
+
+}
